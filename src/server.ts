0b068377a67b514e69880268fb2069d9134d222f
VarunRaj7 <rvarunraj1@gmail.com>
2020-09-23 03:23:57 -0400

    removed cors

--- src/server.ts
+++ src/server.ts
@@ -10,21 +10,10 @@ const logger = createLogger('Root');
 
   app.use(bodyParser.json());
 
-  // CORS Should be restricted
-  app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    next();
-    process.exit(1);
-  });
-
   // Root URI call
   app.get('/', async (req, res) => {
-    res.send('Hello ANT');
     logger.info('connected with browser');
+    res.status(200).send('Hello ANT');
   });
 
   // Start the Server
